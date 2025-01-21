9feb1f64a79158d67c85406b241dda225fc8cd4c
Yurii Demkiv <demkiv123@gmail.com>
2025-01-21 20:31:35 +0200

    add nuxt content and nuxt content config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,10 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   css: ['~/assets/css/tailwind.css'],
-  modules: ['@nuxtjs/tailwindcss', '@nuxt/eslint', '@nuxt/fonts'],
+  modules: [
+    '@nuxt/content',
+    '@nuxtjs/tailwindcss',
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+  ],
 })
